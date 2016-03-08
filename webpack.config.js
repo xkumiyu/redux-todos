7c0fde13058419367374772d4ba758d36ee4aed0
xkumiyu <xkumiyu@gmail.com>
2016-03-08 20:49:19 +0900

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: './dist/',
+    filename: 'index.js',
+    publicPath: '/dist/'
+  },
+  devtool: '#source-map',
+  devServer: {
+    contentBase: './',
+    port: 8080
+  },
+  module: {
+    preLoaders: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'eslint' }
+    ],
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel' }
+    ]
+  },
+  eslint: {
+    configFile: './.eslintrc'
+  }
+};
