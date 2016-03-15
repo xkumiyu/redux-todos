75f238bbde6df30e159880d1dbcc81cc6785ebed
xkumiyu <xkumiyu@gmail.com>
2016-03-15 21:20:39 +0900

    Filter Todo

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,10 +7,16 @@ export const addTodo = (text) => {
   }
 }
 
+export const setVisibilityFilter = (filter) => {
+  return {
+    type: 'SET_VISIBILITY_FILTER',
+    filter
+  }
+}
+
 export const toggleTodo = (id) => {
   return {
     type: 'TOGGLE_TODO',
     id
   }
 }
-  
